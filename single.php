e4b566d7a0d16b8980437adaf256a82096a43e46
heiroa <hlaille.pro@gmail.com>
2021-04-01 10:44:33 -1000

    ajout des commentaire

--- single.php
+++ single.php
@@ -4,6 +4,8 @@
     //Ne pas oublier d'ajouter le fichier Article.php
     require 'Article.php';
     //Ne pas oublier d'ajouter le fichier Comment.php
+    require 'Comment.php';
+
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -31,6 +33,21 @@
                 $articles->closeCursor(); 
             ?> 
             <a href="home.php">Retour à l'accueil</a>
+            <div id="comments" class="text-left" style="margin-left: 50px"> 
+                <h3>Commentaires</h3> 
+                <?php 
+                    $comment = new Comment(); 
+                    $comments = $comment->getCommentsFromArticle($_GET['articleId']); 
+                    while($comment = $comments->fetch()) { 
+                ?>
+                        <h4><?= htmlspecialchars($comment->pseudo);?></h4> 
+                        <p><?= htmlspecialchars($comment->content);?></p> 
+                        <p>Posté le <?= htmlspecialchars($comment->createdAt);?></p> 
+                <?php 
+                    } 
+                    $comments->closeCursor(); 
+                ?> 
+                </div>
         </div>
     </body>
 </html>
