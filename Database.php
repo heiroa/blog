04d7fba0970315040b87857905125e73ca53d981
heiroa <hlaille.pro@gmail.com>
2021-04-01 10:15:51 -1000

    Optimisation du code

--- Database.php
+++ Database.php
@@ -1,19 +1,48 @@
-<?php
-    class Database{
 
+<?php
+
+    abstract class Database{
+        //Nos constantes
         const DB_HOST = 'mysql:host=localhost;dbname=blog;charset=utf8';
         const DB_USER = 'root';
         const DB_PASS = '';
 
-        public function getConnection(){
-            try{
-                $connection = new PDO(self::DB_HOST, self::DB_USER, self::DB_PASS);
-                $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                return $connection;
+        private $connection;
+
+        private function checkConnection(){
+            //Vérifie si la connection est nulle et fait appel à la fonction getConnection
+            if($this -> connection === null){
+                return $this -> getConnection();
             }
+            //Si la connexion existe, elle est renvoyée, inutle de refaire une connexion
+            return $this -> connection;
+        }
+
+        //Méthodes de connection à la base de données 
+        private function getConnection(){
+            //Test de connexion à la base de données
+            try{
+                $this->connection = new PDO(self::DB_HOST, self::DB_USER, self::DB_PASS);
+                $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                //renvoie la connexion
+                return $this->connection;
+            }
+            //Montre l'erreur de connexion
             catch(Exception $errorConnection){
                 die ('Erreur de connection :'.$errorConnection->getMessage());
             }
+        }
+
+        protected function createQuery($sql, $parameters=null){
+            if($parameters){
+                $result = $this -> checkConnection() -> prepare($sql);
+                $result->setFetchMode(PDO::FETCH_CLASS, static::class);
+                $result -> execute($parameters);
+                return $result;
+            }
+            $result = $this -> checkConnection() -> query($sql);
+            $result->setFetchMode(PDO::FETCH_CLASS, static::class);
+            return $result;
         }
     }
 ?>
